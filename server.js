0fd448410d6abcc2923789ed778c17ae01ad1dae
NeonPokharkar <96371679+NeonPokharkar@users.noreply.github.com>
2025-06-02 19:33:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Serve static files
-app.use(express.static(path.join(__dirname, '..', 'dist/launch-edge-frontend/browser')));
+app.use(express.static(path.join(__dirname, '..', 'dist/launch-edge-frontend')));
 
 // Fallback to index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..',  'dist/launch-edge-frontend/browser/index.html'));
+  res.sendFile(path.join(__dirname, '..',  'dist/launch-edge-frontend/index.html'));
 });
 
 app.listen(PORT, () => {
